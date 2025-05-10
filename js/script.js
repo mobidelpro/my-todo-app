48a673725923a05d889a264bb18b75951015c511
Nayira <nayira.yn@gmail.com>
2025-05-10 20:09:45 +0330

    edited stripes

--- js/script.js
+++ js/script.js
@@ -155,7 +155,7 @@ document.addEventListener("DOMContentLoaded", function () {
       ${title}
       <input type="checkbox" checked disabled />
     `;
-    doneItem.style.setProperty("--stripe-color", getPriorityColor(priority));
+    doneItem.classList.add(`stripe-${priority}`);
     tasksDoneList.appendChild(doneItem);
   }
 
